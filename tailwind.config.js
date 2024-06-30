cb1726ca008525e63eb312d24d5149eadee017e7
suryaanshuu <suryanshuc30@gmail.com>
2024-06-30 17:24:13 +0530

    Frontend of QR Application

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+
+    colors: {
+      'green': {
+        1: '#659e38',
+        2: '#8c9d3d',
+        3: '#628b61',
+        4: '#ccdec4'
+      },
+      'gray': {
+        900: '#18181b',
+        1000: '#52525b',
+      },
+      'white': '#ffffff',
+      'black': '#000000',
+      'yellow': {
+        2: '#FFFF00',
+        1: '#FCF55F'},
+    }
+  },
+  variants: {},
+  plugins: [],
+};
